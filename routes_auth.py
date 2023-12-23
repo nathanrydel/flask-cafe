3332143f77c40c5b7e047bfa8351ad4886397ef0
Nathan Rydel <nathanrydel@gmail.com>
2023-12-23 11:56:28 -0700

    updated routes auth

--- routes_auth.py
+++ routes_auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, redirect, session, g
+from flask import Blueprint, session, g
 # from werkzeug.exceptions import Unauthorized
 
-# from config import CURR_USER_KEY, NOT_LOGGED_IN_MSG
-# from models import db, User
+from config import CURR_USER_KEY
+from models import User
 
 bp = Blueprint("auth", __name__)
 
@@ -10,25 +10,25 @@ bp = Blueprint("auth", __name__)
 # auth & auth routes
 
 
-# @bp.before_request
-# def add_user_to_g():
-#     """If we're logged in, add curr user to Flask global."""
+@bp.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
 
-#     if CURR_USER_KEY in session:
-#         g.user = User.query.get(session[CURR_USER_KEY])
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
 
-#     else:
-#         g.user = None
-
-
-# def do_login(user):
-#     """Log in user."""
-
-#     session[CURR_USER_KEY] = user.id
+    else:
+        g.user = None
 
 
-# def do_logout():
-#     """Logout user."""
+def do_login(user):
+    """Log in user."""
 
-#     if CURR_USER_KEY in session:
-#         del session[CURR_USER_KEY]
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
