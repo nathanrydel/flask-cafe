23069b15c4ea9734af47693a6b7438fc82679ca9
Nathan Rydel <nathanrydel@gmail.com>
2023-12-22 18:29:54 -0700

    WTForms template for AddCafeForm

--- forms.py
+++ forms.py
@@ -1,1 +1,16 @@
 """Forms for Flask Cafe."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, URLField, SelectField, PasswordField
+from wtforms.validators import InputRequired, Optional, Email
+
+
+class AddCafeForm(FlaskForm):
+    """Form for adding a cafe"""
+
+    name = StringField("Name", validators=[InputRequired()])
+    description = TextAreaField("Description", validators=[Optional()])
+    url = URLField("URL", validators=[Optional()])
+    address = StringField("Address", validators=[InputRequired()])
+    city = SelectField("City", validators=[InputRequired()])
+    image_url = URLField("Image", validators=[Optional()])
